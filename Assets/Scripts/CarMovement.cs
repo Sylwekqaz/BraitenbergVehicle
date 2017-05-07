79353135d3865110fe74fa0d813b3002af09b769
Sylwekqaz <sylwekqaz19@gmail.com>
2017-05-07 23:25:16 +0200

    Dodanie areny, punktów

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CarMovement : MonoBehaviour
+    {
+        private Rigidbody2D Rigidbody2D { get; set; }
+        public int Speed;
+
+        // OnTriggerEnter2D is called when the Collider2D other enters the trigger (2D physics only)
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log(collision.gameObject.tag);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            Rigidbody2D = GetComponent<Rigidbody2D>();
+            Rigidbody2D.velocity = Vector2.up * Speed;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/PointGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointGenerator : MonoBehaviour
+{
+
+    public GameObject GoodPointsHolder;
+    public GameObject BadPointsHolder;
+
+    public GameObject GoodPoint;
+    public GameObject BadPoint;
+
+    public int BadPointCount;
+    public int GoodPointCount;
+    // Use this for initialization
+    void Start()
+    {
+        Instantiate(GoodPoint, new Vector3(0, 40), Quaternion.identity, GoodPointsHolder.transform);
+
+        for (int i = 0; i < GoodPointCount; i++)
+        {
+            Instantiate(GoodPoint, new Vector3(Random.Range(-50,50), Random.Range(-50, 50)), Quaternion.identity, GoodPointsHolder.transform);
+        }
+
+        for (int i = 0; i < BadPointCount; i++)
+        {
+            Instantiate(BadPoint, new Vector3(Random.Range(-50,50), Random.Range(-50, 50)), Quaternion.identity, BadPointsHolder.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
