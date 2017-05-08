0a7c116e66da3fc3bed9a1b5dfe09fb8a9432b47
Sylwekqaz <sylwekqaz19@gmail.com>
2017-05-08 11:40:16 +0200

    dodanie śledzenie samochodu

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTracking : MonoBehaviour
+{
+    public GameObject Target;
+    private Vector3 _offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        _offset = transform.position - Target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Target.transform.position + _offset;
+    }
+}
