14bb05c793158fa04ee5d5d8058667ab7a5a5dbe
Sylwekqaz <sylwekqaz19@gmail.com>
2017-05-10 11:03:17 +0200

    Dodanie krzyżowania liczb float

---
+++ NeuralLogic/FloatRecombination.cs
@@ -0,0 +1,16 @@
+﻿using Troschuetz.Random;
+
+namespace NeuralLogic
+{
+    public static class FloatRecombination
+    {
+        private static readonly TRandom Random = new TRandom();
+
+        public static float Recombine(float a, float b, float sigma = 0.5f)
+        {
+            var parent = Random.NextBoolean()? a:b; // randomly select parent
+
+            return (float) Random.Normal(parent, sigma);
+        }
+    }
+}
