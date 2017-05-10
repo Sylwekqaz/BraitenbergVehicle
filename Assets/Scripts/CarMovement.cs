8371839ca4badf5ad71a83a4af08046e9284d895
Sylwekqaz <sylwekqaz19@gmail.com>
2017-05-10 16:57:02 +0200

    performance speedup

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -72,7 +72,7 @@ public class CarMovement : MonoBehaviour
         var o = _neuralNet.RunNeuralNet(inputValues);
 
 
-        Debug.LogFormat("{0}   :    {1}", o.LeftWheelMultiplier, o.RightWheelMultiplier);
+//        Debug.LogFormat("{0}   :    {1}", o.LeftWheelMultiplier, o.RightWheelMultiplier);
 
         LeftWheel.GetComponent<Rigidbody2D>().velocity = transform.up * Speed * o.LeftWheelMultiplier;
         RightWheel.GetComponent<Rigidbody2D>().velocity = transform.up * Speed * o.RightWheelMultiplier;
