c526e3c85d54538853e4dc5a31f4415011acc69a
Sylwekqaz <sylwekqaz19@gmail.com>
2017-05-11 17:09:45 +0200

    Zamiana śledzenia kamery

--- Assets/Scripts/CameraTracking.cs
+++ Assets/Scripts/CameraTracking.cs
@@ -4,37 +4,56 @@ using UnityEngine;
 public class CameraTracking : MonoBehaviour
 {
     public GameObject CarsHolder;
-    private GameObject _trackingTarget;
-    private Vector3 _offset;
+    public float DampTime = 0.15f;
+    public float Period = 0.1f;
+    public float SnapDistance = 3f;
 
-    private float nextActionTime = 0.0f;
-    public float period = 0.1f;
+
+
+    private Transform _target;
+    private float _nextActionTime;
+    private Vector3 _velocity = Vector3.zero;
+    private bool _switchingMode;
 
     // Use this for initialization
     private void Start()
     {
-        _trackingTarget = CarsHolder.transform
+        _target = CarsHolder.transform
             .Cast<Transform>()
-            .First()
-            .gameObject;
-
-        var z = transform.position.z - _trackingTarget.transform.position.z;
-        _offset = new Vector3(0,0,z);
+            .First();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Time.time > nextActionTime)
+        if (Time.time > _nextActionTime)
         {
-            nextActionTime += period;
-            _trackingTarget = CarsHolder.transform
+            _nextActionTime += Period;
+            _target = CarsHolder.transform
                 .Cast<Transform>()
-                .Select(t => t.gameObject)
                 .OrderByDescending(o => o.GetComponent<CarMovement>().Points)
                 .First();
+
+            _switchingMode = true;
         }
 
-        transform.position = _trackingTarget.transform.position + _offset;
+        if (!_target) return; 
+
+        var point = GetComponent<Camera>().WorldToViewportPoint(_target.position);
+        var delta = _target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+        var destination = transform.position + delta;
+
+        if (_switchingMode)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref _velocity, DampTime);
+            if (delta.magnitude<SnapDistance)
+            {
+                _switchingMode = false;
+            }
+        }
+        else
+        {
+            transform.position = destination;
+        }
     }
 }
